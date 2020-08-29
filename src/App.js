d76c15c2bdd21c3ef88d3bbb95706df70b0c30e9
Rob Gries <robert.w.gries@gmail.com>
2020-08-29 03:19:48 +0000

    Upload what i got

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { useState } from 'react';
 import './App.css';
-const wasm = import("./native-pogcast/pkg");
+import ffmpeg from './ffmpeg.js/ffmpeg';
+const rust = import("./native-pogcast/pkg");
+
+const mp3Decoder = async (channel) => {
+  const wasm = await ffmpeg();
+  const decoder = new wasm.AVCodecWrapper("mp3");
+  console.log(wasm);
+  decoder.delete();
+
+  const decode = async (path) => {
+    const response = await fetch(path);
+    const inBuf = response.arrayBuffer;
+    let data_size = inBuf.byteLength;
+    let decoded_frame = null;
+    while (data_size > 0) {
+      if (!decoded_frame) {
+        //decoded_frame = wasm.av_frame_alloc();
+        if (!decoded_frame) {
+          console.error("Could not allocate audio frame");
+        }
+      }
+    }
+  };
+
+  return {
+    decode,
+  };
+
+  return async (path) => {
+    const response = await fetch(path);
+    const arrayBuffer = response.arrayBuffer;
+    return;
+  };
+};
 
 function App() {
   const [handle, setHandle] = useState();
+  const bc = new BroadcastChannel('music');
+  const decoder = mp3Decoder(bc);
+  //decoder.decode('../music.mp3');
+
   const start = () => {
-    wasm.then(wasm => {
+    rust.then(wasm => {
       setHandle(wasm.beep());
     });
   };
